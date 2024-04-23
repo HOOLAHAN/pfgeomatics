c95e766f05391c1f6f9e932599e513a56857e2be
Iain <iain.hoolahan@gmail.com>
2024-04-23 22:20:58 +0100

    added servicesData

---
+++ src/data/servicesData.tsx
@@ -0,0 +1,44 @@
+// src/data/servicesData.tsx
+export const servicesData = [
+  {
+    title: "Surveying",
+    image: "surveying.png",
+    service: [
+        "Land Surveying",
+        "Railway Track Monitoring",
+        "Topographical Surveys",
+        "Site Control Establishment & Setting Out",
+        "Primary and Secondary Control",
+        "Grid Lines and Datum Transfer",
+        "Measured Building Surveys",
+        "Structural Monitoring",
+        "Volumetric Surveys for Stockpiles",
+        "3D Laser Scanning"
+    ],
+  },
+  {
+    title: "Setting Out",
+    image: "setting-out.png",
+    service: [
+        "Structural Steelwork",
+        "HDB`s/Steelwork As-builts 2D-3D",
+        "Tekla Structures",
+        "Plumb Line and Level of Steelwork",
+        "RC Frames",
+        "Foundations/Groundworks",
+        "Curtain Walling",
+        "Any Structural and Non-Structural Components"
+    ],
+  },
+  {
+    title: "Other Skills and Services",
+    image: "other-skills-and-services.png",
+    service: [
+        "Pre-Pour Rebar Checks",
+        "AutoCAD",
+        "Excel",
+        "Problem Solving",
+        "Project and Site Team Liaison"
+    ],
+  },
+];
