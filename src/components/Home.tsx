e5182a09becca2ed882390658ff61e26980c0119
Iain <iain.hoolahan@gmail.com>
2024-04-23 20:03:20 +0100

    added About component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,13 @@
 // src/components/Home.tsx
 import { Box } from '@chakra-ui/react';
 import Clients from './Clients';
+import About from './About';
 
 const Home: React.FC = () => {
   return (
     <Box>
-      {/* <About />
-      <Projects />
+      <About />
+      {/* <Projects />
       <Services />
       <Contact /> */}
       <Clients />
