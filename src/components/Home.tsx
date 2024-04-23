735c3fb8f697a79fbfe5a078af583e8de9958a93
Iain <iain.hoolahan@gmail.com>
2024-04-23 22:31:10 +0100

    added Services component and data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,13 +3,14 @@ import { Box } from '@chakra-ui/react';
 import Clients from './Clients';
 import About from './About';
 import Projects from './Projects';
+import Services from './Services';
 
 const Home: React.FC = () => {
   return (
     <Box>
       <About />
       <Projects />
-      {/* <Services /> */}
+      <Services />
       {/* <Contact /> */}
       <Clients />
     </Box>
---
+++ src/components/Services.tsx
@@ -0,0 +1,33 @@
+// src/components/Services.tsx
+import React from 'react';
+import { Box, Heading, UnorderedList, ListItem, SimpleGrid, Image, VStack } from '@chakra-ui/react';
+import { servicesData } from '../data/servicesData';
+
+interface Service {
+  title: string;
+  image: string;
+  service: string[];
+}
+
+const Services: React.FC = () => {
+  return (
+    <Box p={5} maxW="1200px" mx="auto">
+      <Heading as="h2" size="xl" mb={6}>Our Services</Heading>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={10}>
+        {servicesData.map((service: Service) => (
+          <VStack key={service.title} align="stretch" p={5} boxShadow="lg" borderRadius="lg" bg="gray.50">
+            <Image borderRadius="md" src={require(`../data/serviceImages/${service.image}`)} alt={`${service.title} image`} boxSize="200px" objectFit="cover" />
+            <Heading size="lg" mt={4}>{service.title}</Heading>
+            <UnorderedList spacing={2} mt={2}>
+              {service.service.map((item, index) => (
+                <ListItem key={index}>{item}</ListItem>
+              ))}
+            </UnorderedList>
+          </VStack>
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
+};
+
+export default Services;
