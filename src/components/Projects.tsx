6439c89325c0474c1f5a2032c660df33e8716eb2
Iain <iain.hoolahan@gmail.com>
2024-04-25 17:24:13 +0100

    implemented carousel component in Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,10 @@
 // src/components/Projects.tsx
+
 import React, { useState } from 'react';
-import { Box, Image, Text, Heading, HStack, useDisclosure } from '@chakra-ui/react';
+import { Box, Image, Text, Heading, useDisclosure } from '@chakra-ui/react';
 import { projectsData } from '../data/projectsData';
 import ProjectModal from './ProjectModal';
+import ChakraCarousel from './ChakraCarousel';
 
 interface Project {
   name: string;
@@ -25,19 +27,30 @@ const Projects: React.FC = () => {
   };
 
   return (
-    <Box p={5} maxW="1200px" mx="auto">
-      <Heading as="h2" size="xl" mb={6}>Projects</Heading>
-      <HStack spacing={5} overflowX="scroll">
-        {projectsData.map((project: Project) => (
-          <Box key={project.name} onClick={() => handleProjectClick(project)} cursor="pointer">
-            <Image src={require(`../data/projectImages/${project.image}`)} alt={project.name} boxSize="200px" objectFit="cover" />
-            <Text textAlign="center">{project.name}</Text>
-          </Box>
-        ))}
-      </HStack>
-      {selectedProject && (
-        <ProjectModal project={selectedProject} isOpen={isOpen} onClose={onClose} />
-      )}
+    <Box bg="brand.700">
+      <Box p={5} maxW="1200px" mx="auto">
+        <Heading as="h2" size="xl" mb={6}>Projects</Heading>
+        <ChakraCarousel gap={5}>
+          {projectsData.map((project: Project) => (
+            <Box key={project.name} onClick={() => handleProjectClick(project)} cursor="pointer">
+              <Box width="400px" height="200px" overflow="hidden">
+                <Image 
+                  src={require(`../data/projectImages/${project.image}`)}
+                  alt={project.name}
+                  width="100%"
+                  height="100%"
+                  objectFit="cover" 
+                  px={2}
+                />
+              </Box>
+              <Text textAlign="center">{project.name}</Text>
+            </Box>
+          ))}
+        </ChakraCarousel>
+        {selectedProject && (
+          <ProjectModal project={selectedProject} isOpen={isOpen} onClose={onClose} />
+        )}
+      </Box>
     </Box>
   );
 };
