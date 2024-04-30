916a00fb60996852834d469deab958d0e12b80e1
Iain <iain.hoolahan@gmail.com>
2024-04-30 15:41:38 +0100

    on hover images expand. added view icon

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,8 @@
 // src/components/Projects.tsx
 
 import React, { useState } from 'react';
-import { Box, Image, Text, Heading, useDisclosure, useColorModeValue } from '@chakra-ui/react';
+import { Box, Image, Text, Heading, useDisclosure, useColorModeValue, Icon } from '@chakra-ui/react';
+import { ViewIcon } from '@chakra-ui/icons';
 import { projectsData } from '../data/projectsData';
 import ProjectModal from './ProjectModal';
 import ChakraCarousel from './ChakraCarousel';
@@ -33,15 +34,29 @@ const Projects: React.FC = () => {
         <Heading as="h2" size="xl" mb={6} textAlign="center">Projects</Heading>
         <ChakraCarousel gap={5}>
           {projectsData.map((project: Project) => (
-            <Box key={project.name} onClick={() => handleProjectClick(project)} cursor="pointer">
-              <Box width="400px" height="200px" overflow="hidden">
+            <Box key={project.name} onClick={() => handleProjectClick(project)} cursor="pointer"
+              position="relative" overflow="hidden"
+              _hover={{
+                transform: 'scale(1.05)',
+                zIndex: '10',
+                boxShadow: '0 0 8px rgba(0,0,0,0.6)'
+              }}
+            >
+              <Box width="400px" height="200px" position="relative">
                 <Image 
                   src={require(`../media/projectImages/${project.image}`)}
                   alt={project.name}
                   width="100%"
                   height="100%"
-                  objectFit="cover" 
-                  px={2}
+                  objectFit="cover"
+                  transition="all 0.3s ease-in-out"
+                  _hover={{
+                    filter: 'brightness(0.8)'
+                  }}
+                />
+                <Icon as={ViewIcon} color="white" boxSize="4" position="absolute" top="1" right="8"
+                  transition="opacity 0.3s ease"
+                  _groupHover={{ opacity: '1' }}
                 />
               </Box>
               <Text textAlign="center">{project.name}</Text>
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,9 @@
 // src/components/Services.tsx
+
 import React, { useState } from 'react';
-import { Box, Heading, SimpleGrid, Image, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, useDisclosure, Text, useColorModeValue } from '@chakra-ui/react';
+import { Box, Heading, SimpleGrid, Image, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, useDisclosure, Text, useColorModeValue, Icon } from '@chakra-ui/react';
 import { servicesData } from '../data/servicesData';
+import { ViewIcon } from '@chakra-ui/icons';
 
 interface Service {
   title: string;
@@ -25,12 +27,29 @@ const Services: React.FC = () => {
         <Heading as="h2" size="xl" mb={6} textAlign="center">Our Services</Heading>
         <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={10}>
           {servicesData.map((service: Service) => (
-            <Box key={service.title} position="relative" cursor="pointer" onClick={() => handleServiceClick(service)}>
+            <Box key={service.title} position="relative" cursor="pointer" onClick={() => handleServiceClick(service)}
+              overflow="hidden" 
+              _hover={{
+                transform: 'scale(1.05)',
+                zIndex: '10',
+                boxShadow: '0 0 8px rgba(0,0,0,0.6)'
+              }}
+            >
               <Image
                 borderRadius="sm"
                 src={require(`../media/serviceImages/${service.image}`)}
                 alt={`${service.title} image`}
                 objectFit="cover"
+                width="100%"
+                height="100%"
+                transition="all 0.3s ease-in-out"
+                _hover={{
+                  filter: 'brightness(0.8)'
+                }}
+              />
+              <Icon as={ViewIcon} color="white" boxSize="4" position="absolute" top="1" right="3"
+                  transition="opacity 0.3s ease"
+                  _groupHover={{ opacity: '1' }}
               />
               <Heading
                 position="absolute"
@@ -39,6 +58,7 @@ const Services: React.FC = () => {
                 transform="translate(-50%, -50%)"
                 color="white"
                 size="lg"
+                textShadow="1px 1px 3px rgba(0,0,0,0.7)"
               >
                 {service.title}
               </Heading>
