691986019c82279838aad9a009e0d79d3ee93473
Iain <iain.hoolahan@gmail.com>
2024-05-02 11:27:51 +0100

    added logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 // src/components/Navbar.js
 
-import { Box, Flex, Spacer, useColorModeValue } from '@chakra-ui/react';
-import { ColorModeSwitcher } from "../ColorModeSwitcher"
+import { Box, Flex, Image, useColorModeValue } from '@chakra-ui/react';
+import { ColorModeSwitcher } from "../ColorModeSwitcher";
 
 const Navbar = () => {
-  const brandColour = useColorModeValue('lightBrand.400', 'darkBrand.400');
+  const brandColour = useColorModeValue('lightBrand.400', 'darkBrand.800');
 
   return (
     <Box 
@@ -17,13 +17,14 @@ const Navbar = () => {
       top={0}
       zIndex={11}
       width="100%"
-      >
+    >
       <Flex align="center" justify="space-between">
-        <Box fontWeight="bold" fontSize="lg">PF Geomatics</Box>
-        <Spacer />
-        <Flex align="center">
-          <ColorModeSwitcher justifySelf="flex-end" />
-        </Flex>
+        <Image
+          src="/logo.webp" 
+          maxH={{ base: "40px", md: "60px" }}  
+          marginRight="2" 
+        />
+        <ColorModeSwitcher justifySelf="flex-end" />
       </Flex>
     </Box>
   );
