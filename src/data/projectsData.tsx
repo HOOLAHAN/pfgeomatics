d1e7894656522c9f3f6f3a8b8d6b20b6b6fe751a
Iain <iain.hoolahan@gmail.com>
2024-05-07 18:05:50 +0100

    more info added to projectsData

--- src/data/projectsData.tsx
+++ src/data/projectsData.tsx
@@ -3,22 +3,22 @@ export const projectsData = [
   {
     name: "Piccadilly Lights",
     location: "Piccadily, London",
-    client: "???",
+    client: "Severfield UK",
     dateStarted: "2021",
     dateEnded: "2022",
     image: "piccadilly-lights.png",
-    description: "???",
-    linkedIn: "???"
+    description: "Working along the steel frame contractor Severfield, our engineers located components for the steel frame for this flagship project overlooking Piccadilly Circus, including complex roof cassettes that enveloped the building in an innovative design. Many of the cassettes cascaded down the sides of the building, becoming features of multiple floors below. The connection plates between these cassettes and the structure were angled and sloped in many different directions to fit the complex shapes together, requiring three dimensional surveys to be done and 3D drawings to be produved showing their positions relative to design in all planes. Some party walls on the perimeter of the building were listed brickwork and required very minimal modifications to accommodate large steel beams and columns being connected into them, this took precision surveying before and after connection to ensure the existing and new structures levels or integrity were not affected. With many obstacles and re-designs on this project it was critical that onsite information was accurate and relayed as quickly as possible to design teams to avoid delays and additional costs to the project.",
+    linkedIn: ""
   },
   {
     name: "Google Headquarters",
     location: "Kings Cross, London",
-    client: "???",
+    client: "Severfield UK",
     dateStarted: "2019",
     dateEnded: "2023",
     image: "google-headquarters.png",
-    description: "???",
-    linkedIn: "???"
+    description: "Our engineering company played a pivotal role in the construction of the new £1 billion Google Headquarters in Kings Cross, London. Serving as the lead engineers for the steel frame contractor, our managing director led the project for multiple years during the main construction phase. Supported by a dedicated team of engineers, we oversaw every aspect of the steel frame installation, ensuring precise positioning and alignment of every structural element. As the project progressed, our involvement extended beyond the steel framework to include positioning over 30 precast concrete architectural stairs, seamlessly integrating them into the expansive roof garden and walkways surrounding the building's envelope. Our meticulous attention to detail and commitment to excellence contributed to the successful completion of this iconic project, cementing our reputation as industry leaders in engineering innovation and execution.",
+    linkedIn: ""
   },
   {
     name: "Gatwick Station Project",
@@ -34,10 +34,20 @@ export const projectsData = [
     name: "Tempo Project",
     location: "Maidenhead",
     client: "M J Robinson Structures Ltd and ISG",
-    dateStarted: "???",
-    dateEnded: "???",
+    dateStarted: "2022",
+    dateEnded: "2023",
     image: "tempo-project.jpeg",
-    description: "???",
+    description: "Our team provided essential engineering surveys and precision alignment services for structural steelworks, significantly enhancing the project's integrity and success. Working closely with MJ Robinson, we conducted daily detailed surveys, set outs, and established benchmarks, integral to the project's accuracy and structural soundness. Our expertise not only supported MJ Robinson in achieving their construction goals but also contributed to transforming Maidenhead's skyline. This project showcased our commitment to delivering top-tier support and solutions to our clients, reinforcing our position as a leader in construction innovation and collaboration.",
     linkedIn: "https://www.linkedin.com/feed/update/urn:li:activity:7115440233193558016/"
+  },
+  {
+    name: "53 Clarendon Road",
+    location: "Watford, London",
+    client: "Regal London",
+    dateStarted: "2022",
+    dateEnded: "2024",
+    image: "53-clarendon-road.jpg",
+    description: "Regal's The Clarendon project in Watford, a distinguished high-rise residential development, was successfully supported by PF Geomatics over the past year. Our dedicated team of engineering surveyors collaborated closely with the Regal team, playing a pivotal role in overseeing the concrete subcontractor's work. The Clarendon, featuring 168 private residences and amenities such as a 25th-floor sky garden, concierge services, a cinema, and a gymnasium, stands as a cornerstone of Watford’s regeneration efforts. Our responsibilities encompassed conducting rigorous quality checks and rebar inspections, as well as carrying out pre-pour and post-pour surveys crucial for maintaining the tight tolerances required for the cladding works. These surveys ensured that the structure’s slab edges and facade brackets met the precise specifications necessary for successful cladding installation. By overseeing general surveying and setting out verifications, our team ensured that all aspects of the construction adhered to the highest standards, contributing significantly to constructing The Clarendon to the required quality and specifications. This project highlights PF Geomatics` commitment to excellence and our ability to meet complex challenges in urban development.",
+    linkedIn: "https://www.linkedin.com/feed/update/urn:li:activity:7188247696292368384/"
   },
 ];
