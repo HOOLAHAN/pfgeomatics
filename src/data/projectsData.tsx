30819c37810749b78eba0631ff4c91faf3b2ca8a
Iain <iain.hoolahan@gmail.com>
2024-05-10 11:35:42 +0100

    added more projects to data

--- src/data/projectsData.tsx
+++ src/data/projectsData.tsx
@@ -17,7 +17,7 @@ export const projectsData = [
     dateStarted: "2019",
     dateEnded: "2023",
     image: "google-headquarters.png",
-    description: "Our engineering company played a pivotal role in the construction of the new £1 billion Google Headquarters in Kings Cross, London. Serving as the lead engineers for the steel frame contractor, our managing director led the project for multiple years during the main construction phase. Supported by a dedicated team of engineers, we oversaw every aspect of the steel frame installation, ensuring precise positioning and alignment of every structural element. As the project progressed, our involvement extended beyond the steel framework to include positioning over 30 precast concrete architectural stairs, seamlessly integrating them into the expansive roof garden and walkways surrounding the building's envelope. Our meticulous attention to detail and commitment to excellence contributed to the successful completion of this iconic project, cementing our reputation as industry leaders in engineering innovation and execution.",
+    description: "We played a pivotal role in the construction of the new £1 billion Google Headquarters in Kings Cross, London. Serving as the lead engineers for the steel frame contractor, our managing director led the project for multiple years during the main construction phase. Supported by a dedicated team of engineers, we oversaw every aspect of the steel frame installation, ensuring precise positioning and alignment of every structural element. As the project progressed, our involvement extended beyond the steel framework to include positioning over 30 precast concrete architectural stairs, seamlessly integrating them into the expansive roof garden and walkways surrounding the building's envelope. Our meticulous attention to detail and commitment to excellence contributed to the successful completion of this iconic project, cementing our reputation as industry leaders in engineering innovation and execution.",
     linkedIn: ""
   },
   {
@@ -50,4 +50,54 @@ export const projectsData = [
     description: "Regal's The Clarendon project in Watford, a distinguished high-rise residential development, was successfully supported by PF Geomatics over the past year. Our dedicated team of engineering surveyors collaborated closely with the Regal team, playing a pivotal role in overseeing the concrete subcontractor's work. The Clarendon, featuring 168 private residences and amenities such as a 25th-floor sky garden, concierge services, a cinema, and a gymnasium, stands as a cornerstone of Watford’s regeneration efforts. Our responsibilities encompassed conducting rigorous quality checks and rebar inspections, as well as carrying out pre-pour and post-pour surveys crucial for maintaining the tight tolerances required for the cladding works. These surveys ensured that the structure’s slab edges and facade brackets met the precise specifications necessary for successful cladding installation. By overseeing general surveying and setting out verifications, our team ensured that all aspects of the construction adhered to the highest standards, contributing significantly to constructing The Clarendon to the required quality and specifications. This project highlights PF Geomatics` commitment to excellence and our ability to meet complex challenges in urban development.",
     linkedIn: "https://www.linkedin.com/feed/update/urn:li:activity:7188247696292368384/"
   },
+  {
+    name: "4-8 Victoria Street",
+    location: "Victoria, London",
+    client: "LA Metalworks",
+    dateStarted: "2021",
+    dateEnded: "2022",
+    image: "4-8-victoria-street.jpg",
+    description: "This flagship project directly opposite Westminster Abbey consisted a rennovation of an 8 storey RC frame building with two additional steel frame floors added to the roof. The original open atrium in the centre of the building was filled in with steel beams and columns on every level to increase floor area and usable office space for future tenants. The building style was listed and surrounded on either side with Victoria St and Tothill St, making it a complex and high security job with a sensitive public boundary. Our engineers set up primary and secondary control for the project, created the grid system to datums best fit the original structure, carry out high detail surveys of all existing components on every floor for the designers and architects to work to, set-out position and level of new steel members and building strengthening works throughout the project. We worked closely with the site team to detect design clashes before elements were installed and were continuously relaying new information to the designers and client as the demolition of obsolete existing structure was being done. ",
+    linkedIn: ""
+  },
+  {
+    name: "Castle Lane",
+    location: "Victoria, London",
+    client: "LA Metalworks",
+    dateStarted: "2021",
+    dateEnded: "2022",
+    image: "castle-lane.jpg",
+    description: "This large rennovation project of 5 buildings a short walk from Victoria tube station required detailed surveys of all existing structural and non-structural brickwork and RC walls. The buildings were originally residential with lots of small rooms with between 5 and 7 levels in each. This meant the surveys required lots of transferred control and instrument setups to complete to a high standard and many months of continuous surveying to get the information to the design teams in good time. Each building had one to two steel frame levels added to the roof so steel components and strengthening works needed to be set out to a high degree of precision to fit the existing structure. Our engineers worked tirelessly alongside the main contractor to collect data and release detailed drawings to ensure designers and other contractors had the information required to keep working and avoid any costly delays.",
+    linkedIn: ""
+  },
+  {
+    name: "40 Eastbourne Terrace",
+    location: "Paddington, London",
+    client: "Harrogate Steel",
+    dateStarted: "2022",
+    dateEnded: "2023",
+    image: "40-eastbourne-terrace.jpg",
+    description: "This project right next to the Elizabeth line entrance at Paddington Station comprised the strengthening of an existing 5 level RC building and adding two steel frame levels to the top. Our engineers had to survey every RC component of the existing structure and relay this information to the site and design teams. This building being over 70m long and each floor containing hundreds of columns made it a challenging task requiring lots of internal and external control points and setups to achieve. Our engineers also set-out the steel frame on top by marking the precise locations of the existing RC columns from the floor below to ensure the new load was transferred most effectively through the existing structure.",
+    linkedIn: ""
+  },
+  {
+    name: "HS2",
+    location: "Coptle North Tunnel, West Ruislip",
+    client: "Skansa, Costain, Strabag",
+    dateStarted: "2022",
+    dateEnded: "2024",
+    image: "HS2.jpg",
+    description: "One of our engineers has been working as a Senior Engineering Surveyor on the HS2 Copthall North Tunnel. This work includes monitoring the excavations of the cut and fill tunnel, monitoring the tunnel structure after construction and during backfill, carrying out pre and post-pour RC concrete surveys and setting up and transferring control throughout the site. The monitoring works involve use of robotic total stations, laser scanning machines and piezometers in a three-pronged attack to ensure structural soundness and safety of the tunnel.",
+    linkedIn: ""
+  },
+  {
+    name: "40 Tower Hill",
+    location: "Tower Hill, London",
+    client: "CR Construction",
+    dateStarted: "2024",
+    dateEnded: "2024",
+    image: "40-tower-hill.jpg",
+    description: "This refurbishment project at 40 Tower Hill for our client CR Construction involved comprehensive control checks and detailed surveying of existing levels. Our engineers meticulously carried out these tasks to ensure precision and accuracy throughout the project. We were also responsible for setting out gridlines and datums for the blockwork team, ensuring that every aspect of their work was aligned perfectly with the project's requirements. Additionally, our team marked out structural openings for the demolition team, facilitating a smooth and efficient process.",
+    linkedIn: ""
+  }
 ];
