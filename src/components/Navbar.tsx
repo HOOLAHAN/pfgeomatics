57bad2d9d3e6831ec4b4d26c7c18493947d49fb6
Iain <iain.hoolahan@gmail.com>
2024-05-13 16:08:28 +0100

    navbar links in dropdown menu on phone sized screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,20 @@
 // src/components/Navbar.tsx
 
-import { Box, Flex, Image, useColorModeValue, Link as ChakraLink, useBreakpointValue } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Box, Flex, IconButton, Image, Link as ChakraLink, useColorModeValue, useBreakpointValue, Stack, Collapse } from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import { ColorModeSwitcher } from "../ColorModeSwitcher";
 import { Link } from 'react-scroll';
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const brandColour = useColorModeValue('lightBrand.400', 'darkBrand.800');
 
   // Use Chakra UI's useBreakpointValue to handle responsive offsets
-  const offset = useBreakpointValue({ base: -56, md: -76 });
+  const baseOffset = isOpen ? -256 : -56;
+  const offset = useBreakpointValue({ base: baseOffset, md: -56 });
+
+  const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
     <Box 
@@ -23,13 +29,15 @@ const Navbar: React.FC = () => {
       width="100%"
       m={0}
     >
-      <Flex align="center" justify="space-between">
-        <Image
-          src="/logo.webp" 
-          maxH={{ base: "40px", md: "60px" }}  
-          marginRight="2" 
-        />
-        <Flex>
+      <Flex align="center" justify="space-between" position="relative">
+        <ColorModeSwitcher />
+        <Box position="absolute" left="50%" transform="translateX(-50%)">
+          <Image
+            src="/logo.webp" 
+            maxH={{ base: "40px", md: "40px" }}  
+          />
+        </Box>
+        <Flex display={{ base: "none", md: "flex" }}>
           <Link to="cover-video" smooth={true} duration={500} offset={offset}>
             <ChakraLink px={2}>Home</ChakraLink>
           </Link>
@@ -46,8 +54,36 @@ const Navbar: React.FC = () => {
             <ChakraLink px={2}>Clients</ChakraLink>
           </Link>
         </Flex>
-        <ColorModeSwitcher justifySelf="flex-end" />
+        <IconButton
+          display={{ base: "flex", md: "none" }}
+          onClick={toggleMenu}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          variant="ghost"
+          aria-label="Toggle Navigation"
+          color="white"
+        />
       </Flex>
+      <Collapse in={isOpen} animateOpacity>
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as="nav" spacing={4}>
+            <Link to="cover-video" smooth={true} duration={500} offset={offset} onClick={toggleMenu}>
+              <ChakraLink>Home</ChakraLink>
+            </Link>
+            <Link to="projects" smooth={true} duration={500} offset={offset} onClick={toggleMenu}>
+              <ChakraLink>Projects</ChakraLink>
+            </Link>
+            <Link to="services" smooth={true} duration={500} offset={offset} onClick={toggleMenu}>
+              <ChakraLink>Services</ChakraLink>
+            </Link>
+            <Link to="contact" smooth={true} duration={500} offset={offset} onClick={toggleMenu}>
+              <ChakraLink>Contact</ChakraLink>
+            </Link>
+            <Link to="clients" smooth={true} duration={500} offset={offset} onClick={toggleMenu}>
+              <ChakraLink>Clients</ChakraLink>
+            </Link>
+          </Stack>
+        </Box>
+      </Collapse>
     </Box>
   );
 }
