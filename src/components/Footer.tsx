e7f3538c53ed0274cdd59a1ada09dbdd38491a78
Iain <iain.hoolahan@gmail.com>
2024-05-15 14:43:18 +0100

    horizontal stacking of links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,33 +1,43 @@
 // src/components/Footer.tsx
 
-import { Box, Text, Link, VStack, Container, HStack, Icon, useDisclosure } from '@chakra-ui/react';
+import { Box, Text, Link, Flex, Icon, useDisclosure, useBreakpointValue, Container } from '@chakra-ui/react';
 import { FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import PrivacyPolicy from './PrivacyPolicy';
 
 const Footer: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  // Determine the flex direction based on screen size
+  const flexDirection = useBreakpointValue<'column' | 'row'>({ base: 'column', md: 'row' });
+
   return (
     <Box bg="gray.200" color="gray.700" mt={10} py={5}>
       <Container maxW="container.xl">
-        <VStack spacing={2}>
-          <Text fontSize="lg" fontWeight="bold">PFGeomatics</Text>
-          <Text>© 2024 PFGeomatics. All rights reserved.</Text>
-          <HStack spacing={1}>
-            <Icon as={FaEnvelope} w={5} h={5} color="blue.500" />
-            <Link href="mailto:info@pfgeomatics.co.uk" isExternal color="blue.500">
-              info@pfgeomatics.co.uk
-            </Link>
-          </HStack>
-          <HStack spacing={1}>
-            <Icon as={FaLinkedin} w={5} h={5} color="blue.500" />
-            <Link href="https://www.linkedin.com/company/pf-geomatics/" isExternal color="blue.500">
-              LinkedIn
-            </Link>
-          </HStack>
-          <Link href="#" onClick={onOpen} color="blue.500">Privacy Policy</Link>
+        <Flex direction="column" align="center">
+          <Text fontSize="lg" fontWeight="bold">PF Geomatics</Text>
+          <Text>© 2024 PF Geomatics. All rights reserved.</Text>
+          <Flex
+            direction={flexDirection}
+            align="center"
+            justify="center"
+            wrap="wrap"
+          >
+            <Flex align="center" m={2}>
+              <Icon as={FaEnvelope} w={5} h={5} color="blue.500" mr={2} />
+              <Link href="mailto:info@pfgeomatics.co.uk" isExternal color="blue.500">
+                info@pfgeomatics.co.uk
+              </Link>
+            </Flex>
+            <Flex align="center" m={2}>
+              <Icon as={FaLinkedin} w={5} h={5} color="blue.500" mr={2} />
+              <Link href="https://www.linkedin.com/company/pf-geomatics/" isExternal color="blue.500">
+                LinkedIn
+              </Link>
+            </Flex>
+            <Link href="#" onClick={onOpen} color="blue.500" m={2}>Privacy Policy</Link>
+          </Flex>
           <PrivacyPolicy isOpen={isOpen} onClose={onClose} />
-        </VStack>
+        </Flex>
       </Container>
     </Box>
   );
