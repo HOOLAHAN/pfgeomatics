78d1c305cd89077fa804da9fb9113b83db3ae934
Iain <iain.hoolahan@gmail.com>
2024-05-30 11:31:18 +0100

    created clientData file to store information about the clients

---
+++ src/data/clientData.tsx
@@ -0,0 +1,59 @@
+// src/data/clientData.tsx
+export const clientData = [
+  {
+    name: "Severfield UK",
+    website: "https://www.severfield.com/",
+    logo: "severfield-logo.png",
+    about: "Severfield plc is the largest specialist structural steelwork Group in the UK and among the biggest in Europe. They manufacture and install fabricated steelwork throughout the UK and Europe in the commercial, industrial, nuclear and infrastructure sectors.",
+    pfgService: "On Severfield projects, we conduct detailed surveys of both existing and new structures to ensure the precise positioning of fabricated steelwork for their projects. We meticulously set-out the locations of structural components, draw detailed as-built drawings, and collaborate closely with site and design teams to identify and resolve any clashes, ensuring the project progresses smoothly to completion."
+  },
+  {
+    name: "Kilnbridge",
+    website: "https://www.kilnbridge.com/",
+    logo: "kilnbridge-logo.png",
+    about: "Kilnbridge is an engineering and construction business. Operating as a turnkey specialist structures contractor, they undertake enabling works, complex structural alterations, civil engineering and infrastructure works, reinforced concrete basements, sub and superstructure frame construction and structural steelwork.",
+    pfgService: "On Kilnbridge projects, our comprehensive surveying services support their specialist structures projects by providing thorough surveys of existing and new structures. We accurately set out structural component locations, create detailed as-built records of surveyed elements, and work in partnership with site and design teams to detect and address potential clashes, helping bring the project to a successful conclusion."
+  },
+  {
+    name: "Lindner Prater",
+    website: "https://www.lindnerprater.co.uk/",
+    logo: "lindner-prater-logo.png",
+    about: "Lindner Prater is the UK’s leading specialist building envelope contractor, a single source supplier capable of providing complete solutions from design and planning to manufacture and installation.",
+    pfgService: "We perform meticulous surveys of the structure and produce comprehensive as-built drawings to aid design, ensure the precise installation of building envelope and facade components on reinforced concrete and steel structures. We collaborates with site and design teams to identify and rectify any clashes or design issues between trades."
+  },
+  {
+    name: "8 Build",
+    website: "https://www.8build.com/",
+    logo: "8build-logo.png",
+    about: "8 Build's construction division is made up of highly-experienced teams that cover everything from light to heavy refurbishments, as well as new builds.",
+    pfgService: "Our team provides detailed surveys of existing structures for new projects, precisely mark out locations for demolition works and new RC and steel structural components. We produce precise as-built drawings, and work closely with site and design teams to detect and resolve any clashes, ensuring the project is successfully delivered."
+  },
+  {
+    name: "Regal London",
+    website: "https://www.regal.co.uk/",
+    logo: "regal-london-logo.png",
+    about: "Regal are one of London’s leading residential-led mixed-use developers, creating beautiful and inspiring places, from homes to workspaces, student accommodation to later living communities – and all the spaces in between.",
+    pfgService: "For Regal, we offer detailed surveying services that encompass both existing and new structures, ensuring precise placement of structural components for residential-led mixed-use developments. We create thorough as-built records, set out accurate locations for structural elements, and collaborate with site and design teams to identify and mitigate clashes, ensuring the project is completed seamlessly."
+  },
+  {
+    name: "Alpine Group",
+    website: "https://alpinegroupuk.com/",
+    logo: "",
+    about: "AlpineGroup specialise in the design, manufacture and installation of beautiful feature staircases, balustrades and railings for luxury residential, commercial and landmark addresses across the UK.",
+    pfgService: "We carry out detailed surveys of the locations on site surrounding these bespoke luxury stairways which often span over multiple floors and take up a large footprint. Surveying every floor level, stair entry point, structural component and feature to send to the design team for them to design perfectly to suit."
+  },
+  {
+    name: "MJ Robinson",
+    website: "https://www.mjrobinson.com/",
+    logo: "MJ-Robinson-Structures.png",
+    about: "MJ Robinson Structures carry out the design, detailing, fabrication & erection of high quality structural steelwork, which has established them as one of the leading steelwork companies in the industry.",
+    pfgService: "We conduct detailed surveys of both existing and new structures to guarantee accurate design of structural steelwork. We set out precise locations for structural components, draw detailed as-builts, and work in tandem with site and design teams to identify and resolve any clashes, ensuring the project reaches successful completion."
+  },
+  {
+    name: "Capital Steel Structures",
+    website: "https://www.capitalsteelstructures.co.uk/",
+    logo: "",
+    about: "",
+    pfgService: ""
+  },
+];
