d5cc914e8ae067eb5c8762aab51c1001853725ba
Iain <iain.hoolahan@gmail.com>
2025-02-27 20:51:59 +0000

    logo font colour changes with colour mode

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,10 @@
 // src/components/About.tsx
 
 import React, { useState } from 'react';
-import { Box, Heading, useColorModeValue, Button, VStack, useMediaQuery, Grid, Image, Flex, Spinner, Center } from '@chakra-ui/react';
+import { 
+  Box, Heading, useColorModeValue, Button, VStack, useMediaQuery, 
+  Grid, Image, Flex, Spinner, Center 
+} from '@chakra-ui/react';
 import AboutModal from './AboutModal';
 
 // Import an image for the About section
@@ -12,57 +15,44 @@ const About: React.FC = () => {
   const buttonBorderColor = useColorModeValue('black', 'white');
   const buttonTextColor = useColorModeValue('black', 'white');
   const buttonHoverBg = useColorModeValue('gray.200', 'whiteAlpha.300');
+  const logoSrc = useColorModeValue('/PFG_LOGO_B2.png', '/PFG_LOGO_W2.png'); 
+
   const [isModalOpen, setModalOpen] = useState(false);
   const [isSmallScreen] = useMediaQuery('(max-width: 600px)');
   const [loading, setLoading] = useState(true);
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
-  
-  const handleImageLoad = () => {
-    setLoading(false);
-  };
+  const handleImageLoad = () => setLoading(false);
 
   return (
     <Box bg={brandColour}>
       <Box p={5} mx="auto" maxW="1200px">
-        <Grid
-          templateColumns={{ base: '1fr', md: '1fr 1fr' }}
-          gap={6}
-          alignItems="center"
-        >
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            height={{ base: 'auto', md: '100%' }}
-            width={{ base: '100%', md: 'auto' }}
-            position="relative"
-          >
+        <Grid templateColumns={{ base: '1fr', md: '1fr 1fr' }} gap={6} alignItems="center">
+          <Flex justifyContent="center" alignItems="center" height={{ base: 'auto', md: '100%' }} width={{ base: '100%', md: 'auto' }} position="relative">
             {loading && (
-              <Spinner
-                size="xl"
-                position="absolute"
-                top="50%"
-                transform="translate(-50%, -50%)"
-                zIndex="10"
-              />
+              <Spinner size="xl" position="absolute" top="50%" transform="translate(-50%, -50%)" zIndex="10" />
             )}
-            <Center><Image
-              src={aboutImage}
-              alt="About us"
-              borderRadius="sm"
-              objectFit="cover"
-              boxSize={{ base: '300px', md: '500px' }}
-              onLoad={handleImageLoad}
-              display={loading ? 'none' : 'block'}
-            /></Center>
+            <Center>
+              <Image
+                src={aboutImage}
+                alt="About us"
+                borderRadius="sm"
+                objectFit="cover"
+                boxSize={{ base: '300px', md: '500px' }}
+                onLoad={handleImageLoad}
+                display={loading ? 'none' : 'block'}
+              />
+            </Center>
           </Flex>
           <Box px={10} textAlign="center">
-          <Center><Image
-            src="/PFG_LOGO_W2.png"
-            alt="PF Geomatics Logo"
-            maxH={{ base: '100px', md: '150px' }}
-            /></Center>
+            <Center>
+              <Image
+                src={logoSrc}
+                alt="PF Geomatics Logo"
+                maxH={{ base: '100px', md: '150px' }}
+              />
+            </Center>
             <Heading as="h1" size="xl" mb={4}>
               We are a site engineering and surveying company based in London
             </Heading>
@@ -73,13 +63,8 @@ const About: React.FC = () => {
                 variant="outline"
                 borderColor={buttonBorderColor}
                 color={buttonTextColor}
-                _hover={{
-                  bg: buttonHoverBg,
-                }}
-                _active={{
-                  bg: buttonHoverBg,
-                  transform: 'scale(0.95)',
-                }}
+                _hover={{ bg: buttonHoverBg }}
+                _active={{ bg: buttonHoverBg, transform: 'scale(0.95)' }}
                 transition="all 0.2s ease-in-out"
               >
                 About
