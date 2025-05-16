ea3dabe37bc3407b29a826ccdbeebc75eab9e9dc
Iain <iain.hoolahan@gmail.com>
2025-05-16 22:54:19 +0100

    responsive buttons on click/hover

--- src/components/ChakraCarousel/Carousel.tsx
+++ src/components/ChakraCarousel/Carousel.tsx
@@ -133,6 +133,8 @@ useEffect(() => {
         position="relative"
         mr={2}
         bg={brandBg}
+        _hover={{ bg: 'brand.100' }}
+        _active={{ bg: 'brand.200', transform: 'scale(0.95)' }}
       />
         {[...Array(repetitions)].map((_, index) => (
           <Flex key={`${id}-${index}`} overflowX="hidden" overflowY="visible">
@@ -164,6 +166,8 @@ useEffect(() => {
         position="relative"
         ml={2}
         bg={brandBg}
+        _hover={{ bg: 'brand.100' }}
+        _active={{ bg: 'brand.200', transform: 'scale(0.95)' }}
       />
     </Flex>
   );
