2722c72905945ba91f62bd4a131ac008cf69b100
Iain <iain.hoolahan@gmail.com>
2025-05-16 21:26:20 +0100

    carousel type changes for narrow screens

--- src/components/sections/Projects/Projects.tsx
+++ src/components/sections/Projects/Projects.tsx
@@ -1,6 +1,6 @@
 // src/components/Projects.tsx
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Box,
   Heading,
@@ -11,6 +11,9 @@ import ProjectModal from './ProjectModal';
 import { Carousel, Direction } from '../../ChakraCarousel';
 import ProjectCard from '../../ChakraCarousel/CarouselCard';
 import { getFirstImage } from '../../../utils/checkImageExists';
+import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
 
 interface Project {
   name: string;
@@ -26,6 +29,17 @@ interface Project {
 const Projects: React.FC = () => {
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+        const checkScreenSize = () => {
+          setIsMobile(window.innerWidth < 720);
+        };
+
+        checkScreenSize();
+        window.addEventListener('resize', checkScreenSize);
+        return () => window.removeEventListener('resize', checkScreenSize);
+      }, []);
 
   const handleProjectClick = (project: Project) => {
     setSelectedProject(project);
@@ -43,30 +57,68 @@ const Projects: React.FC = () => {
     <Box 
     py={5}
     >
-      <Box px={0} maxW="1200px" mx="auto">
+      <Box px={0} maxW="1335px" mx="auto">
         <Heading as="h2" size="xl" mb={6} textAlign="center" color="brand.800">
           Featured Projects
         </Heading>
 
-        <Carousel
-          id="project-carousel"
-          interval={6000}
-          direction={Direction.RIGHT}
-          repetitions={1}
-          items={carouselItems}
-        >
-          <ProjectCard
-            key="carousel-item"
-            title=""
-            description=""
-            image={{ imageUrl: '' }}
-            onClick={() => {}}
-            id=""
-            index={0}
-            slides={projectsData.length}
-          />
-        </Carousel>
-
+        {isMobile ? (
+          <ResponsiveCarousel
+            showThumbs={false}
+            infiniteLoop
+            autoPlay
+            interval={6000}
+            showStatus={false}
+            showIndicators={false}
+            emulateTouch
+          >
+            {carouselItems.map((item, index) => (
+              <Box
+                key={index}
+                cursor="pointer"
+                onClick={item.onClick}
+                borderRadius="md"
+                overflow="hidden"
+                px={4} py={2}
+              >
+                <img
+                  src={item.image?.imageUrl}
+                  alt={item.title}
+                  style={{
+                    width: '100%',
+                    maxWidth: '360px',
+                    margin: '0 auto',
+                    height: '200px',
+                    objectFit: 'cover',
+                    borderRadius: '8px',
+                  }}
+                />
+                <Heading size="md" mt={2} textAlign="center" color="brand.800">
+                  {item.title}
+                </Heading>
+              </Box>
+            ))}
+          </ResponsiveCarousel>
+        ) : (
+          <Carousel
+            id="project-carousel"
+            interval={6000}
+            direction={Direction.RIGHT}
+            repetitions={1}
+            items={carouselItems}
+          >
+            <ProjectCard
+              key="carousel-item"
+              title=""
+              description=""
+              image={{ imageUrl: '' }}
+              onClick={() => {}}
+              id=""
+              index={0}
+              slides={projectsData.length}
+            />
+          </Carousel>
+        )}
         {selectedProject && (
           <ProjectModal project={selectedProject} isOpen={isOpen} onClose={onClose} />
         )}
